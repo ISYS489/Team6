2cd799dbb64862cd7e72ab99fce021338dbcac2f
thompk20 <thompk20@ferris.edu>
2014-02-25 06:48:57 -0800

    Update editaccountsettings.php

--- Website/editaccountsettings.php
+++ Website/editaccountsettings.php
@@ -1,15 +1,4 @@
 <html>
-<!-- included for testing purposes -->
-
-<ul class ="nav">
-<li><a href="index.php">Home</a></li>
-<li><a href="eventlist.php">Event List</a></li>
-<li><a href="search.php">Search Events</a></li>
-<li><a href="http://www.ferris.edu/pep/">PEP Page</a></li>
-<li><a href="login.php">Login/Register</a></li>
-<li><a href="about.php">About</a></li>
-<li><a href="accounttools.php">Account Tools</a></li>
-</ul>
 
 <!-- link to style sheet -->
 <link rel="stylesheet" type="text/css" href="mystyle.css">
@@ -17,6 +6,7 @@
 <head>
 <!--header-->
 <?php require 'header.php'; ?>
+
 <h1>Edit Account</h1>
 </head>
 
