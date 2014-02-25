cededf7a502ab6ba9764f0c1039ccf506c251d4b
thompk20 <thompk20@ferris.edu>
2014-02-25 06:42:20 -0800

    Update register.php

--- Website/register.php
+++ Website/register.php
@@ -1,25 +1,26 @@
 <html>
+
 <!-- link to style sheet -->
 <link rel="stylesheet" type="text/css" href="mystyle.css">
+
 <head>
 
 <?php include("header.html");?>
 
-</head> 
+</head>
 <body>
 
 <h1>Register Here</h1>
 <form class ="login" id="login-register" method="post" action="index.php">
 <h2>New User Register Here</h2>
-			
-			<input type="text" placeholder="First Name" name="firstname" autofocus />
-			<br></br>
-			<input type="text" placeholder="Last Name" name="lastname" autofocus />
-			<br></br>
-			<button type="submit">Register</button>
-            <span></span>
+<input type="text" placeholder="First Name" name="firstname" autofocus />
+<br></br>
+<input type="text" placeholder="Last Name" name="lastname" autofocus />
+<br></br>
+<button type="submit" >Register</button>
+<span></span>
 
-        </form>
+</form>
 
 
 </body>
