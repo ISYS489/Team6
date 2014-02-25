5cbc4a35f19d42a8f02072ccbe6d3bfc4b40dc05
thompk20 <thompk20@ferris.edu>
2014-02-25 05:50:02 -0800

    updated

--- Website/about.php
+++ Website/about.php
@@ -1,14 +1,22 @@
 <html>
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
 <head>
 
 <?php include("header.html");?>
 
-</head> 
+</head>
+
+
 <body>
 
 <h1>About Us</h1>
 
-<p>My first paragraph.</p>
+<p>This site debates civility issues. <br>
+Unregistered guests are welcome to browse all the content. <br>
+Users that would like to post,comment, and rate events must register.</p>
 
 </body>
 </html> 
