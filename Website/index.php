9cbaa3048262949ecab955840d2921d6899e91e1
thompk20 <thompk20@ferris.edu>
2014-02-25 05:51:41 -0800

    Update index.php

--- Website/index.php
+++ Website/index.php
@@ -1,14 +1,25 @@
 <html>
+
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
 <head>
+<!--header-->
+<?php require 'header.php'; ?>
+<h1>Civility In US Politics</h1>
+</head>
 
-<?php include("header.html");?>
-
-</head> 
 <body>
-
 <h1>Welcome to team 6's Site</h1>
+<p>
+<!--image-->
+<img src="civ.png" alt="civility_image">
 
-<p>My first paragraph.</p>
+<br>
+<br>
+This is our civility site
+</p>
 
 </body>
 </html> 
