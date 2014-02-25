a1a0136c0812caad9fe084219dc4d1a3b2205c75
thompk20 <thompk20@ferris.edu>
2014-02-25 06:47:35 -0800

    Update search.php

--- Website/search.php
+++ Website/search.php
@@ -1,27 +1,56 @@
 <html>
 
+
 <!-- link to style sheet -->
 <link rel="stylesheet" type="text/css" href="mystyle.css">
-
 <head>
 
 <?php include("header.html");?>
 
+
 </head>
+
 <body>
 
-<h1>Register Here</h1>
-<form class ="login" id="login-register" method="post" action="index.php">
-<h2>New User Register Here</h2>
-<input type="text" placeholder="First Name" name="firstname" autofocus />
+<h1>The Search Page</h1>
+
+<p>
+<form class ="search" id="search" method="post" action="index.php">
+
+<h2>Enter Search Criteria Here</h2>
+
+<input type="text" placeholder="Search" name="search" autofocus />
 <br></br>
-<input type="text" placeholder="Last Name" name="lastname" autofocus />
+
+<button type="submit">Search</button>
 <br></br>
-<button type="submit" >Register</button>
-<span></span>
+
+<h2> Advanced Search</h2>
+
+<input type="text" placeholder="Event Name " name="eventname" autofocus />
+<br></br>
+
+<input type="text" placeholder="Rating " name="rating" autofocus />
+<br></br>
+
+<input type="text" placeholder="Media Type " name="mediatype" autofocus />
+<br></br>
+
+<input type="text" placeholder="Course # " name="coursenumber" autofocus />
+<br></br>
+
+<input type="text" placeholder="Political Party" name="politicalparty" autofocus />
+<br></br>
+
+<input type="text" placeholder="University " name="university" autofocus />
+<br></br>
+
+<button type="submit">Search</button>
+<br></br>
+
 
 </form>
-
+</p>
 
 </body>
 </html> 
