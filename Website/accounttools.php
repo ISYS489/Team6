a43d10fdc0afa845b1515433028ca16118c520f8
thompk20 <thompk20@ferris.edu>
2014-02-25 05:58:13 -0800

    Create accounttools.php

---
+++ Website/accounttools.php
@@ -0,0 +1,61 @@
+<html>
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+<head>
+<!--header-->
+<?php require 'header.php'; ?>
+
+<h1>Account Tools</h1>
+</head>
+
+<body>
+
+<p>
+<h1>Welcome to Your Account Tools</h1>
+
+<!-- student -->
+<ul class ="Student">
+<li><a href="">my posts</a></li>
+<li><a href="">class posts</a></li>
+</ul>
+
+<!-- professor-->
+<ul class ="Professor">
+<li><a href="">Add Keywords</a></li>
+<li><a href="">Create Course </a></li>
+<li><a href="">Deactivate Course </a></li>
+<li><a href="">Create User</a></li>
+<li><a href="">Deactivate User</a></li>
+<li><a href="">View Reports</a></li>
+</ul>
+
+
+<!-- university admin-->
+<ul class ="UniversityAdmin">
+<li><a href="">Add Keywords</a></li>
+<li><a href="">Create Course </a></li>
+<li><a href="">Deactivate Course </a></li>
+<li><a href="">Create User</a></li>
+<li><a href="">Deactivate User</a></li>
+<li><a href="">View Reports</a></li>
+</ul>
+
+<!-- site admin -->
+<ul class ="SiteAdmin">
+<li><a href="">Add Keywords</a></li>
+<li><a href="">Create Course </a></li>
+<li><a href="">Deactivate Course </a></li>
+<li><a href="">Create User</a></li>
+<li><a href="">Deactivate User</a></li>
+<li><a href="">View Reports</a></li>
+<li><a href="">Create University</a></li>
+<li><a href="">Deactivate University</a></li>
+</ul>
+
+
+</p>
+
+</body>
+</html> 
