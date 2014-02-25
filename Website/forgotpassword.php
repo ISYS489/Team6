72f1eedb28d6d56845f942a08c0f59c24d5728fe
thompk20 <thompk20@ferris.edu>
2014-02-25 08:13:50 -0800

    Create forgotpassword.php

---
+++ Website/forgotpassword.php
@@ -0,0 +1,22 @@
+<html>
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+<head>
+<!--header-->
+<?php require 'header.php'; ?>
+<h1>Forgot Password</h1>
+</head>
+
+<body>
+<h2></h2>
+<p>
+
+<button type="submit" >Submit</button>
+
+
+</p>
+
+</body>
+</html> 
