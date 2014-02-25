ec6c5a2556efab4db4e015acedd116c1e784c346
thompk20 <thompk20@ferris.edu>
2014-02-25 05:56:38 -0800

    Update search.php

--- Website/search.php
+++ Website/search.php
@@ -1,15 +1,55 @@
 <html>
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 <head>
 
 <?php include("header.html");?>
 
-<link rel="stylesheet" type="text/css" href="mystyle.css">
-</head> 
+
+</head>
+
 <body>
 
-<h1>search</h1>
+<h1>The Search Page</h1>
 
-<p>My first paragraph.</p>
+<p>
+<form class ="search" id="search" method="post" action="index.php">
+
+<h2>Enter Search Criteria Here</h2>
+
+<input type="text" placeholder="Search" name="search" autofocus />
+<br></br>
+
+<button type="submit">Login</button>
+<br></br>
+
+<h2> Advanced Search</h2>
+
+<input type="text" placeholder="Event Name " name="eventname" autofocus />
+<br></br>
+
+<input type="text" placeholder="Rating " name="rating" autofocus />
+<br></br>
+
+<input type="text" placeholder="Media Type " name="mediatype" autofocus />
+<br></br>
+
+<input type="text" placeholder="Course # " name="coursenumber" autofocus />
+<br></br>
+
+<input type="text" placeholder="Political Party" name="politicalparty" autofocus />
+<br></br>
+
+<input type="text" placeholder="University " name="university" autofocus />
+<br></br>
+
+<button type="submit">Login</button>
+<br></br>
+
+
+</form>
+</p>
 
 </body>
 </html> 
