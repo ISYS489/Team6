f8fc6e246a9ac196df9e759f8b73ef65460be746
thompk20 <thompk20@ferris.edu>
2014-02-25 08:12:54 -0800

    Create forgotusername.php

---
+++ Website/forgotusername.php
@@ -0,0 +1,23 @@
+<html>
+
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+<head>
+<!--header-->
+<?php require 'header.php'; ?>
+<h1>Forgot Username</h1>
+</head>
+
+<body>
+<h2></h2>
+<p>
+
+<button type="submit" >Submit</button>
+
+
+</p>
+
+</body>
+</html> 
