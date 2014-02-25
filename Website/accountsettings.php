dad329a244fbd46159d8e325e3e484c4c9f5000e
thompk20 <thompk20@ferris.edu>
2014-02-25 06:44:52 -0800

    Create accountsettings.php

---
+++ Website/accountsettings.php
@@ -0,0 +1,37 @@
+<html>
+<!-- included for testing purposes -->
+
+<ul class ="nav">
+<li><a href="index.php">Home</a></li>
+<li><a href="eventlist.php">Event List</a></li>
+<li><a href="search.php">Search Events</a></li>
+<li><a href="http://www.ferris.edu/pep/">PEP Page</a></li>
+<li><a href="login.php">Login/Register</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="accounttools.php">Account Tools</a></li>
+</ul>
+
+<!-- link to style sheet -->
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+<head>
+<!--header-->
+<?php require 'header.php'; ?>
+
+
+<h1>Account Settings</h1>
+</head>
+
+<body>
+<p>
+<h2>Account Information</h2>
+</p>
+
+<h2>Edit Account Settings</h2>
+<ul class ="editaccountsettings">
+<li><a href="editaccountsettings.php">Edit Account Settings</a></li>
+</ul>
+
+
+</body>
+</html> 
