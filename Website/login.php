654bb4ec28f486039acddbf6f4bb77fdd6aa023b
cale168 <cale_168@hotmail.com>
2014-02-26 18:59:14 -0500

    working on login

--- Website/login.php
+++ Website/login.php
@@ -13,11 +13,23 @@
 
 <head>
 
-<?php include("header.html");?>
+<?php include("header.html");
+// print any error messages
+if (isset($errors)) && !empty($errors)) {
+	echo '<h1> Error! </h1>
+	<p> The follwing error(s) occurred:<br />';
+	foreach ($errors as $msg){
+		echo " - $msg<br />\n";
+	}
+	echo '</p><p>Please try again.</p>'
+}
+
+
+?>
 
 </head>
 <body>
-
+///display login form
 <h1>Login Or Register</h1>
 <form class ="login" id="login-register" method="post" action="index.php">
 
@@ -38,7 +50,7 @@
 >>>>>>> 6088d2ca7e5483f4f48a564bbd06c3d6bdc6e96c
 =======
 >>>>>>> 6088d2ca7e5483f4f48a564bbd06c3d6bdc6e96c
-<h2> If your a new user click register</h2>
+<h2> If you are a new user click register</h2>
 <ul class ="register">
 <a href="register.php">Register</a>
 <span></span>
