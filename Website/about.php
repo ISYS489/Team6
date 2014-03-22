6789f9368920cf1be405ca3c2244c09dd7921574
Spitfire1900 <spitfire1900@live.com>
2014-03-22 10:25:41 -0400

    Updated about.php to be more descriptive

--- Website/about.php
+++ Website/about.php
@@ -18,7 +18,7 @@ session_start();
 
 <h1>About Us</h1>
 
-<p>This site debates civility issues. <br>
+<p>This site debates civility issues and provides ratings on various events in history. <br>
 Unregistered guests are welcome to browse all the content. <br>
 Users that would like to post,comment, and rate events must register.</p>
 
