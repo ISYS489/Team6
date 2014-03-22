cddd5638041786007fa62dd94a041b8e7419b582
Kyle Gottfried <gottfrk@ferris.edu>
2014-03-22 01:22:56 -0400

    deactivateuniversity.php now lists all active universities.

--- Website/deactivateuniversity.php
+++ Website/deactivateuniversity.php
@@ -7,21 +7,35 @@ session_start();
 
 
 <head>
-<!--header-->
+    <!--header-->
 
-<?php require 'header.php'; ?>
+    <?php require 'header.php';
+          require ('mysqli_connect.php');
+    ?>
 
 
 </head>
 
 <body>
-<h1>Deactivate a University</h1>
-<form id="deactivateUniversityForm" method="post">
-Select University to Deactivate:<br/>
-<select name="university">
-<option value="1">University 1</option>
-</select><br/>
-<button type="submit" onclick="window.confirm('Are you sure you want to delete this University?')" >Delete University</button>
-</form>
+    <h1>Deactivate a University</h1>
+    <form id="deactivateUniversityForm" method="post">
+        Select University to Deactivate:
+        <br />
+        <select name="university">
+            <?php
+            
+            $result = mysqli_query($dbc,'SELECT UniversityId, Name FROM universities WHERE inactive <> 1 OR inactive IS NULL');
+            
+            while ($row=mysqli_fetch_array($result))
+            {
+                echo '<option value=' . htmlspecialchars($row['UniversityId']) . '">'
+                . htmlspecialchars($row['Name'])
+                . '</option>';
+            }
+            ?>
+        </select>
+        <br />
+        <button type="submit" onclick="window.confirm('Are you sure you want to delete this University?')">Delete University</button>
+    </form>
 </body>
-</html> 
+</html>
