2f590f9bd5445437c832de4df5a859164e86174a
Spitfire1900 <spitfire1900@live.com>
2014-03-22 02:17:52 -0400

    Universities are now deactivateable. However, code still needs to be put in place to deactivate all associated courses.

--- Website/deactivateuniversity.php
+++ Website/deactivateuniversity.php
@@ -17,21 +17,55 @@ session_start();
 </head>
 
 <body>
+
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $errors = array(); //Initialize an error array.
+
+        if (!is_null($_POST['universityId']))
+        {
+            $universityId = $_POST['universityId'];
+            
+            $queryString = "UPDATE `universities` SET `inactive`=true WHERE `UniversityId`='$universityId';";
+            
+            $command = @mysqli_query($dbc, $queryString); //run query
+            
+            if ($command) {//if it ran ok
+
+                //print message:
+                echo '<h1>Thank You!</h1>
+			<p>You have deactivated a University.</p>';
+
+            }else{ //if not ok
+
+                echo '<h1>Error</h1>
+			<p>System error preventing University deactivation.</p>';
+            }
+        }
+    }
+
+    ?>
+
+
     <h1>Deactivate a University</h1>
     <form id="deactivateUniversityForm" method="post">
         Select University to Deactivate:
         <br />
-        <select name="university">
+        <select name="universityId">
             <?php
-            
+
             $result = mysqli_query($dbc,'SELECT UniversityId, Name FROM universities WHERE inactive <> 1 OR inactive IS NULL');
-            
+
             while ($row=mysqli_fetch_array($result))
             {
-                echo '<option value=' . htmlspecialchars($row['UniversityId']) . '">'
+                echo '<option value=' . htmlspecialchars($row['UniversityId']) . '>'
                 . htmlspecialchars($row['Name'])
                 . '</option>';
             }
+
+            mysqli_close($dbc);
             ?>
         </select>
         <br />
