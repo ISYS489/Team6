96a19fcfccdf8204d274115cbadf1b80668396f0
cale168 <cale_168@hotmail.com>
2014-03-29 16:24:49 -0400

    updates

--- Website/logout.php
+++ Website/logout.php
@@ -1,13 +1,13 @@
 <?php //logs out user by clearing session variable
 
-//starts session
-session_start();
-
-$_SESSION = array(); //clears out session variables.
-session_destroy(); //Destroys session itself.
-setcookie ('PHPSESSID', '', time()-3600, '/', '', 0, 0); //destroy the cookie.
-
-require ('loginFunctions.php');
-redirect_user();                //sends user to homepage.
+	//starts session
+	session_start();
+	
+	$_SESSION = array(); //clears out session variables.
+	session_destroy(); //Destroys session itself.
+	setcookie ('PHPSESSID', '', time()-3600, '/', '', 0, 0); //destroy the cookie.
+	
+	require ('loginFunctions.php');
+	redirect_user();                //sends user to homepage.
 
 ?>
