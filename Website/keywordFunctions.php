3a9139758663a4d3d0f9359699bd58cad522f54c
cale168 <cale_168@hotmail.com>
2014-03-29 17:44:55 -0400

    minor change

--- Website/keywordFunctions.php
+++ Website/keywordFunctions.php
@@ -2,70 +2,7 @@
 // this page defines functions used to display keywords
 
 
-/**
-* //redirects user to index for any website.
-* //for this particular website the passed argument is the index.
-* function redirect_user ($page = 'index.php'){
-* 	//define URL
-* 	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-* 	
-* 	//remove any trailing slashes
-* 	$url = rtrim($url, '/\\');
-* 	
-* 	// Add the page:
-* 	$url .= '/' . $page;
-* 	
-* 	// Redirect User:
-* 	header("location: $url");
-* 	exit(); // Stops the script
-* 	
-* }
-
-* function check_login($dbc, $username = '', $password = ''){
-* 	
-* 	$errors = array(); //creates error array
-* 	
-* 	//validate email adress
-* 	if (empty($username)) {
-* 		$errors[] = 'You forgot to enter your email address.';
-* 	}else{
-* 		$u = mysqli_real_escape_string($dbc, trim($username));
-* 	}
-* 	
-* 	//validate password
-* 	if (empty($password)) {
-* 		$errors[] = 'You forgot to enter your password.';
-* 	}else{
-* 		$p = mysqli_real_escape_string($dbc, trim($password));
-* 	}
-* 	
-* 	if (empty($errors)) { // if there are no errors
-* 		
-* 		//retrieve userid and first name
-* 		$q = "SELECT userid, firstname FROM users WHERE username='$u' AND password=('$p')";
-* 		$r = @mysqli_query ($dbc, $q);
-* 		// run query
-* 		
-* 		//check result
-* 		if (mysqli_num_rows($r) == 1){
-* 			
-* 			//fetch record
-* 			$row = mysqli_fetch_array ($r, MYSQLI_ASSOC);
-* 			
-* 			//return true
-* 			return array(true, $row);
-* 			
-* 		}else{
-* 			$errors[] = 'The username and password entered do not match those on file.';
-* 		}
-* 		
-* 		//return false and errors
-* 		return array(false, $errors);
-* 	}
-* }
-*/
-
-
+//Used on addKeyword.php, createEvent.php
 function displayKeywords(){
  	require ('mysqliConnect.php');
 		/* multi query statement */
