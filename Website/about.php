989582e07eb781675ce9fe59187922000ac0a5f7
thompk20 <thompk20@ferris.edu>
2014-03-30 17:02:34 -0400

    Update about.php

--- Website/about.php
+++ Website/about.php
@@ -19,8 +19,12 @@ session_start();
 <h1>About Us</h1>
 
 <p>This site debates civility issues and provides ratings on various events in history. <br>
+<br>
 Unregistered guests are welcome to browse all the content. <br>
-Users that would like to post,comment, and rate events must register.</p>
+<br>
+Users that would like to post,comment, and rate events must register.<br>
+<br>
+Thank you for taking the time to visit our site. </p>
 
 </body>
 </html> 
