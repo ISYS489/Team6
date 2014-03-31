3275062c79f0bc684386d61bf33ad73e4bf671ac
Spitfire1900 <spitfire1900@live.com>
2014-03-31 17:12:23 -0400

    Create University, solve multiple database connections error.

--- Website/createUniversity.php
+++ Website/createUniversity.php
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	if (empty($errors)) { //if there are no errors
 		//connect to the DB
-		require ('mysqliConnect.php');
+		//require ('mysqliConnect.php');
 		//make the query
 		$q = "INSERT INTO universities (name, startdate, enddate, IsActive) VALUES ('$un','$sd','$ed', true)";
 		$r = @mysqli_query ($dbc, $q); //run query
