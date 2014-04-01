c35084e3c10dddb231fd76f75438dff150726db4
cale168 <cale_168@hotmail.com>
2014-03-31 22:19:52 -0400

    Added comments and indentation

--- Website/accountSettings.php
+++ Website/accountSettings.php
@@ -1,14 +1,19 @@
 <?php
+//File Name: accountSettings.php
+//Purpose: This page displays information on record for a logged in user.
+//Class: ISYS489
+//Instructor: Amy Buse
+//Author: Cale Kuchnicki
+//Last Date Modified: 3/30/2014
+//
+
+//start the session
 session_start();
 ?>
-//start the session
 
 <html>
 
-
-
 <head>
-
 
 <?php require 'header.php'; ?>
 
@@ -18,37 +23,29 @@ session_start();
 <body>
 <p>
 <h2>Account Information</h2>
-
-<font color="white">
-
-<?php require ('mysqliConnect.php');
-
-$userID = $_SESSION['userid'];
-
-$userQuery = "SELECT * FROM users WHERE userid='$userID'";
-$r = @mysqli_query ($dbc, $userQuery); //run query
-
-while($row = mysqli_fetch_array($r))
-  {
-  	//echo 'Active Course #: ' . $row['FirstName'] . '</br>';
-	//echo 'University: ' . $row['FirstName'] . '</br>';
-	echo 'First Name    : ' . $row['FirstName'] . '</br>';
-	echo 'Middle Initial: ' . $row['MiddleInitial'] . '</br>';
-	echo 'Last Name     : ' . $row['LastName'] . '</br>';
-	echo 'Email Address : ' . $row['Email'] . '</br>';
-	echo 'Date Created  : ' . $row['CreationDate'] . '</br>';
-	echo 'Activity Status: ' . $row['IsActive'] . '</br>';
-  }
-	
-
-
-
-mysqli_close($dbc);
-
-?>
-</font>
-
-
+	<font color="white">
+		<?php require ('mysqliConnect.php');
+		
+			$userID = $_SESSION['userid'];
+			
+			$userQuery = "SELECT * FROM users WHERE userid='$userID'";
+			$r = @mysqli_query ($dbc, $userQuery); //run query
+			
+			while($row = mysqli_fetch_array($r))
+			  {
+			  	//echo 'Active Course #: ' . $row['FirstName'] . '</br>';
+				//echo 'University: ' . $row['FirstName'] . '</br>';
+				echo 'First Name    : ' . $row['FirstName'] . '</br>';
+				echo 'Middle Initial: ' . $row['MiddleInitial'] . '</br>';
+				echo 'Last Name     : ' . $row['LastName'] . '</br>';
+				echo 'Email Address : ' . $row['Email'] . '</br>';
+				echo 'Date Created  : ' . $row['CreationDate'] . '</br>';
+				echo 'Activity Status: ' . $row['IsActive'] . '</br>';
+			  }
+				
+			mysqli_close($dbc);
+		?>
+	</font>
 </p>
 
 
