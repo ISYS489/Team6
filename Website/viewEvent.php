99def7b6ffc12259414f9eabd5208160bf721909
Spitfire1900 <spitfire1900@live.com>
2014-04-08 00:39:44 -0400

    viewEvent added from FTP site.

---
+++ Website/viewEvent.php
@@ -0,0 +1,80 @@
+<?php
+//File Name: myPosts.php
+//Purpose: Page lists events.
+//Class: ISYS489
+//Instructor: Amy Buse
+//Author: Kyle Thompson
+//Last Date Modified: 3/28/2014
+
+
+//start the session & set logged in user's ID
+session_start();
+$userID = $_SESSION['userId'];
+?>
+
+
+<html>
+
+<head>
+
+    <?php include("header.php");?>
+
+</head>
+<body>
+
+    <h1>View Event</h1>
+
+  <?php
+
+DEFINE ('DB_USER', 'isys489c_thompk');
+DEFINE ('DB_PASSWORD', 'q8K[A4LJDd&]');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'isys489c_brteam6');
+
+//Make Connection   @= hide errors                                    die will terminate function of the script
+$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  
+  $whatever = $_GET['eid'];
+  $result = mysqli_query($dbc, "SELECT events.eventid, events.eventname, events.publishdate, events.eventdesc, events.politicalpartyid, events.mediatypeid, events.newsoutletid, users.username
+   FROM events INNER JOIN users ON events.userid = users.userid where events.eventid = $whatever");
+  
+
+
+
+echo "<table border='1'>
+<tr>
+<th> event id</th>
+<th>event name</th>
+<th> publish date</th>
+<th> event description</th>
+<th> political party id</th>
+<th> media type id</th>
+<th> news outlet id </th>
+<th> user name  </th>
+</tr>";
+
+
+  while($row = mysqli_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['eventid'] . "</td>";
+  echo "<td>" . $row['eventname'] . "</td>";
+  echo "<td>" . $row['publishdate'] . "</td>";
+  echo "<td>" . $row['eventdesc'] . "</td>";
+  echo "<td>" . $row['politicalpartyid'] . "</td>";
+  echo "<td>" . $row['mediatypeid'] . "</td>";
+  echo "<td>" . $row['newsoutletid'] . "</td>";
+  echo "<td>" . $row['username'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+mysqli_close($dbc);
+?>
+
+</body>
+</html>
