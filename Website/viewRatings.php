ce8a3ef17bf153fa8352e51ec9a467448c6c5fd7
Spitfire1900 <spitfire1900@live.com>
2014-04-13 21:01:58 -0400

    Add viewRatings.php

---
+++ Website/viewRatings.php
@@ -0,0 +1,27 @@
+<?php
+//File Name: viewRatings.php
+//Purpose: This page displays ratings reports
+//Class: ISYS489
+//Instructor: Amy Buse
+//Author: Kyle Gottfried
+//Last Date Modified: 4/13/2014
+
+//start the session
+session_start();
+?><html>
+
+
+
+<head>
+<!-- navigation bar -->
+<?php
+include("header.php");
+require("mysqliConnect.php");
+?>
+
+</head>
+<body>
+    <h1>Event List</h1>
+<table>
+</table>
+</body>
