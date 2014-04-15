eefedc0ed6810a91c006476f486363fb64cd0521
thompk20 <thompk20@ferris.edu>
2014-04-15 13:14:45 -0400

    Update search.php

--- Website/search.php
+++ Website/search.php
@@ -25,46 +25,39 @@ session_start();
 
 <h1>The Search Page</h1>
 
-<p>
-<form class ="search" id="search" method="post" action="searchResults.php">
+<p align="center">
+<form class ="search" align="center" id="search" method="post" action="searchResults.php">
 
-<h2>Enter Search Criteria Here</h2>
 
-<input type="text" placeholder="Search" name="search" autofocus />
-<br></br>
-
-<button type="submit">Search</button>
-<br></br>
-
-<h2> Advanced Search</h2>
+<h2 align="center" >Search</h2>
 
 <?php
 	
-	require ('keywordFunctions.php');
+	//require ('keywordFunctions.php');
 	
 	//uses post variables 'media_type', 'name', 'political_party', 'news_outlet',  
-	displayKeywords();
+	//displayKeywords();
 ?>
 
-<input type="text" placeholder="Event Name " name="eventname" autofocus />
+<input align="center" type="text" placeholder="Event Name " name="eventname"  autofocus />
 <br></br>
 
-<input type="text" placeholder="Rating " name="rating" autofocus />
+<input type="text" placeholder="Rating " name="rating" align="center" autofocus />
 <br></br>
 
-<input type="text" placeholder="Media Type " name="mediatype" autofocus />
+<input type="text" placeholder="Media Type " name="mediatype" align="center" autofocus />
 <br></br>
 
-<input type="text" placeholder="Course # " name="coursenumber" autofocus />
+<input type="text" placeholder="Course # " name="coursenumber" align="center" autofocus />
 <br></br>
 
-<input type="text" placeholder="Political Party" name="politicalparty" autofocus />
+<input type="text" placeholder="Political Party" name="politicalparty" align="center" autofocus />
 <br></br>
 
-<input type="text" placeholder="University " name="university" autofocus />
+<input type="text" placeholder="University " name="university" align="center" autofocus />
 <br></br>
 
-<button type="submit">Search</button>
+<button type="submit" align="center" >Search</button>
 <br></br>
 
 
