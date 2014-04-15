e378689832d2127e752f527d5437217dc4a5c6ea
thompk20 <thompk20@ferris.edu>
2014-04-15 13:30:07 -0400

    Update searchResults.php

--- Website/searchResults.php
+++ Website/searchResults.php
@@ -20,72 +20,62 @@ require 'mysqliConnect.php';
 
 <?php
 
-//DEFINE ('DB_USER', 'isys489c_thompk');
-//DEFINE ('DB_PASSWORD', 'q8K[A4LJDd&]');
-//DEFINE ('DB_HOST', 'localhost');
-//DEFINE ('DB_NAME', 'isys489c_brteam6');
-
-////Make Connection   @= hide errors                                    die will terminate function of the script
-//$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
-//if (mysqli_connect_errno())
-//  {
-//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//  }
-
-//$results = mysqli_query($dbc,"SELECT a.eventid, a.eventname, a.publishdate, a.eventdesc, b.politicalparty, c.mediatype, d.newsoutlet, e.rating, e.comment
- //FROM events a
- //full outer join politicalparties b
- //on a.politicalpartyid = b.politicalpartyid 
- //full outer join mediatypes c
- //full outer join newsoutlets d
- //on a.newsoutletid = d.newsoutletid
- //full outer join ratings e
- //on a.eventid = e.eventid
- //Where a.eventname like '%{$_POST['eventname']}%', or e.rating like '%{$_POST['rating']}%', or c.mediatype like '%{$_POST['mediatype']}%', or b.politicalparty like '%{$_POST['politicalparty']}%'");
  
-$result = mysqli_query($dbc,"SELECT eventid, eventname, publishdate, eventdesc, politicalpartyid, mediatypeid, newsoutletid
-from events
-where eventname like '%{$_POST['eventname']}%'");
+$result = mysqli_query($dbc,"SELECT  DISTINCT e.EventId, e.EventName, date(e.PublishDate) AS PublishDate, e.DateOfEvent, p.PoliticalParty, nO.NewsOutlet, m.MediaType, n.Name, avg(a.rating) as rating, z.name, q.classid
+FROM events AS e
+LEFT OUTER JOIN politicalparties AS p ON e.PoliticalPartyId = p.PoliticalPartyId
+LEFT OUTER JOIN newsoutlets AS nO ON e.NewsOutletId = nO.NewsOutletId
+LEFT OUTER JOIN names AS n ON e.NameId = n.NameId
+LEFT OUTER JOIN mediatypes AS m ON e.MediaTypeId = m.MediaTypeId
+left outer join ratings as a on e.eventid = a.eventid
+left outer join users as g on e.userid = g.userid
+left outer join universities as z on g.UniversityId = z.UniversityId
+left outer join classes as q on g.universityid = q.universityid
+WHERE e.isvisible=true and e.eventname = '{$_POST['eventname']}' or a.rating = '{$_POST['rating']}' or m.MediaType = '{$_POST['mediatype']}' or p.PoliticalParty = '{$_POST['politicalparty']}' or z.Name = '{$_POST['university']}' or q.classid = '{$_POST['coursenumber']}'
+group by e.EventName");
+//where eventname = '{$_POST['search']}'");
 
-echo "<table border='1'>
+
+echo "<table border='1' class='searchlist' align='center'>
 <tr>
-<th> event id</th>
-<th>event name</th>
-<th> publish date</th>
-<th> event description</th>
-<th> political party id</th>
-<th> media type id</th>
-<th> news outlet id </th>
-<th> rating </th>
-<th> comment </th>
-<th> click below to view event</th>
+<th>Event Name</th>
+<th> Publish Date</th>
+<th> Date of Event</th>
+<th> Political Party</th>
+<th>Person of interest</th>
+<th> Media Type</th>
+<th> News Outlet</th>
+<th>Rating</th>
+<th>University</th>
+<th>Course Number</th>
+<th> click below to view event </th>
 </tr>";
 
 
   while($row = mysqli_fetch_array($result))
+  
   {
   echo "<tr>";
-  echo "<td>" . $row['eventid'] . "</td>";
-  echo "<td>" . $row['eventname'] . "</td>";
-  echo "<td>" . $row['publishdate'] . "</td>";
-  echo "<td>" . $row['eventdesc'] . "</td>";
-  echo "<td>" . $row['politicalpartyid'] . "</td>";
-  echo "<td>" . $row['mediatypeid'] . "</td>";
-  echo "<td>" . $row['newsoutletid'] . "</td>";
+  echo "<td>" . $row['EventName'] . "</td>";
+  echo "<td>" . $row['PublishDate'] . "</td>";
+  echo "<td>" . $row['DateOfEvent'] . "</td>";
+  echo "<td>" . $row['PoliticalParty'] . "</td>";
+  echo "<td>" . $row['Name'] . "</td>";
+  echo "<td>" . $row['MediaType'] . "</td>";
+  echo "<td>" . $row['NewsOutlet'] . "</td>";
   echo "<td>" . $row['rating'] . "</td>";
-  echo "<td>" . $row['comment'] . "</td>";
-  echo "<td>" . "<a href=\"viewEvent.php?eid=".$row['eventid']."\" id='eventlist'>Click Here To View This Event</a>" . "</td>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['classid'] . "</td>";
+  echo "<td>" . "<a href=\"viewEvent.php?eid=".$row['EventId']."\" id='eventlist' >Click Here To View This Event</a>" . "</td>";
   echo "</tr>";
   }
+  
+  
 echo "</table>";
+
+
 mysqli_close($dbc);
 ?> 
-
-
-
-
-
-
 
 
 <body>
