1bef5a0620eaefd3106cd0a69923c488b1774857
cale168 <cale_168@hotmail.com>
2014-04-22 19:41:24 -0400

    Works now

--- Website/classPosts.php
+++ Website/classPosts.php
@@ -27,38 +27,44 @@ $userID = $_SESSION['userid'];
 <body>
 
     <h1>Class Posts</h1>
+	<div class="myposts">
   <?php
 
 
+$result = mysqli_query($dbc, "SELECT uc.classid FROM users u left outer join `users-classes` as uc on uc.userId = u.userId WHERE u.userid=" . $userID);
+while($row = mysqli_fetch_array($result))
+  {
+   $classID = $row['classid'];
+  }
+
+$query = "SELECT DISTINCT e.EventId, e.EventName, uc.ClassId, c.ClassName, u.UserId, u.username, e.PublishDate, e.DateOfEvent, pp.PoliticalParty, mt.MediaType, no.NewsOutlet, n.Name
+  from events as e
+  left outer join users as u on u.UserId = e.UserId
+  left outer join `users-classes` as uc on uc.userId = e.userId
+  left outer join classes as c on c.classid = uc.classid
+  left outer join universities as uv on u.UniversityId = uv.UniversityId
+  left outer join politicalparties as pp on e.PoliticalPartyId = pp.PoliticalPartyId
+  left outer join mediatypes as mt on e.MediaTypeId = mt.MediaTypeId
+  left outer join newsoutlets as no on e.NewsOutletId = no.NewsOutletId
+  left outer join names as n on e.NameId = n.NameId
+  where e.isvisible = 1 AND c.classid =" . $classID;
   
-  
-  $result = mysqli_query($dbc, "SELECT a.ClassId, a.ClassName, d.UserId, e.EventId, e.EventName, e.PublishDate, e.DateOfEvent, f.PoliticalParty, g.MediaType, h.NewsOutlet, i.Name
-  from classes as a
-  left outer join universities as b on a.UniversityId = b.UniversityId
-  left outer join `users-classes` as c on a.ClassId = c.ClassId
-  left outer join users as d on c.UserId = d.UserId
-  left outer join events as e on d.UserId = e.UserId
-  left outer join politicalparties as f on e.PoliticalPartyId = f.PoliticalPartyId
-  left outer join mediatypes as g on e.MediaTypeId = g.MediaTypeId
-  left outer join newsoutlets as h on e.NewsOutletId = h.NewsOutletId
-  left outer join names as i on e.NameId = i.NameId
-  where d.UserId = $userID");
-
-
-
+  $result = mysqli_query($dbc, $query);
+   
+		
 echo "<table border='1'>
-<tr>
-<th>class id</th>
-<th>class name</th>
-<th>User id</th>
-<th>event id</th>
-<th>event name</th>
-<th> publish date</th>
-<th> date of event/th>
-<th> political party</th>
-<th>media type</th>
-<th> news outlet</th>
-<th> name </th>
+<tr bgcolor='9B1321' >
+<th>Class ID</th>
+<th>Class Name</th>
+<th>Username</th>
+<th>Event ID</th>
+<th>Event Name</th>
+<th> Publish Date</th>
+<th> Date of Event</th>
+<th> Political Party</th>
+<th> Media Type</th>
+<th> News Outlet</th>
+<th> Name </th>
 <th> click here to view event</th>
 </tr>";
 
@@ -68,16 +74,16 @@ echo "<table border='1'>
   echo "<tr>";
   echo "<td>" . $row['ClassId'] . "</td>";
   echo "<td>" . $row['ClassName'] . "</td>";
-    echo "<td>" . $row['UserId'] . "</td>";
+    echo "<td>" . $row['username'] . "</td>";
 	  echo "<td>" . $row['EventId'] . "</td>";
 	    echo "<td>" . $row['EventName'] . "</td>";
 		  echo "<td>" . $row['PublishDate'] . "</td>";
-		    echo "<td>" . $row['DateOfEvet'] . "</td>";
+		    echo "<td>" . $row['DateOfEvent'] . "</td>";
 			  echo "<td>" . $row['PoliticalParty'] . "</td>";
 			    echo "<td>" . $row['MediaType'] . "</td>";
 				  echo "<td>" . $row['NewsOutlet'] . "</td>";
 				    echo "<td>" . $row['Name'] . "</td>";
-  echo "<td>" . "<a href=\"viewEvent.php?eid=".$row['eventid']."\" id='eventlist'>Click Here To View This Event</a>" . "</td>";
+  echo "<td>" . "<a href=\"viewEvent.php?eid=".$row['EventId']."\" id='eventlist'>Click Here To View This Event</a>" . "</td>";
   echo "</tr>";
   }
   
@@ -85,7 +91,7 @@ echo "<table border='1'>
 echo "</table>";
 
 ?>
-
+</div>
 
 </body>
 </html>
