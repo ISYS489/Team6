7b50521d3f928324e9e0c7a3d1189ed990c620d0
cale168 <cale_168@hotmail.com>
2014-05-03 14:41:07 -0400

    Update reports.php

--- Website/reports.php
+++ Website/reports.php
@@ -13,7 +13,10 @@ session_start();
 <html>
 
 <head>
-	<?php require 'header.php'; ?>
+	<?php
+		//display header info
+		require 'header.php';
+	?>
 	
 	<h1>Welcome to the Report Page</h1>
 </head>
@@ -23,6 +26,8 @@ session_start();
 <div id="reportsdiv">
 <p>
 
+
+<!--displays links to different reports-->
 <br>
 <a href='viewUserReports.php'>View User Report</a><br><br>
 <a href='viewRatingReports.php'>View Rating Report</a><br><br>
